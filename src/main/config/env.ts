cd810df56961ac0fc5ef34348a9b9b00b2f3d94e
argas7 <argas@cin.ufpe.br>
2022-02-04 17:10:45 -0300

    feat: implements discord integration

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -17,24 +17,15 @@ export const env = {
   httpServer: {
     port: parseInt(process.env.API_PORT as string, 10) || (3001 as number),
   },
-  databases: {
-    postgres: {
-      url: process.env.DATABASE_URL as string,
-    },
-  },
   logs: {
     sentry: {
       url: process.env.SENTRY_URL as string,
     },
   },
-  cloud: {
-    cognito: {
-      apiVersion: process.env.COGNITO_API_VERSION as string,
-      region: process.env.COGNITO_REGION as string,
-      accessKeyId: process.env.COGNITO_ACCESS_KEY_ID as string,
-      secretAccessKey: process.env.COGNITO_SECRET_ACCESS_KEY as string,
-      clientId: process.env.COGNITO_CLIENT_ID as string,
-      userPoolId: process.env.COGNITO_USER_POOL_ID as string,
+  bot: {
+    discordToken: process.env.DISCORD_TOKEN as string,
+    channels: {
+      general: process.env.GENERAL_CHANNEL_ID as string,
     },
   },
 };
