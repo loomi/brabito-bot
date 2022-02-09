fea049fc4b1d7bd0f3f76eb092e67b7078548bbe
argas7 <argas@cin.ufpe.br>
2022-02-09 08:22:38 -0300

    feat: updgrade controller to handle with webhook calls

--- src/application/http-server/controllers/project-update-controller.ts
+++ src/application/http-server/controllers/project-update-controller.ts
@@ -1,12 +1,21 @@
 import { Controller, HttpResponse } from '@/application/http-server/protocols';
 
 import { ok, serverError } from '@/application/http-server/helpers';
+import { PrWebhookHandler } from '@/domain/pr/usecases/pr-webhook-handler/protocols';
 
 export class ProjectUpdateController implements Controller {
+  private readonly prsWebhookHandler: PrWebhookHandler;
+  constructor(prsWebhookHandler: PrWebhookHandler) {
+    this.prsWebhookHandler = prsWebhookHandler;
+  }
+
   async handle(httpRequest: any): Promise<HttpResponse> {
     try {
-      console.log('--------------------------------------------------------');
+      console.log('=========================================================');
       console.log(httpRequest);
+      console.log('=========================================================');
+      await this.prsWebhookHandler.createOrUpdatePr(httpRequest);
+
       return ok();
     } catch (error) {
       const catchedError = error as Error;
