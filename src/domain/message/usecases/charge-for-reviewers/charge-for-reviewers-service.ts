28d8e4134fd1fa30cd4a10679ad26578901a358d
argas7 <argas@cin.ufpe.br>
2022-02-15 19:15:22 -0300

    feat: add githubLink when charge for reviews

--- src/domain/message/usecases/charge-for-reviewers/charge-for-reviewers-service.ts
+++ src/domain/message/usecases/charge-for-reviewers/charge-for-reviewers-service.ts
@@ -76,6 +76,7 @@ export class ChargeForReviewersService implements ChargeForReviewersUsecase {
           createdAt,
           projectName,
           userGithubNick,
+          githubLink,
           discordId,
           user,
         } = currPr.toJSON();
@@ -91,38 +92,38 @@ export class ChargeForReviewersService implements ChargeForReviewersUsecase {
 
           const messagesForNotAllocated = {
             important: [
-              `Já desisti de vcs viu <@&805765119384616962>, se aloquem ou não, não vou ficar mais de :clown: aqui...\nps: se existir alguma alma caridosa, seguem os dados: PR **${discordId}** de **${projectName}** aberto por **${userGithubNick}**`,
-              `<@&805765119384616962>, preciso de alguém pra corrigir o PR **${discordId}** lá em **${projectName}**`,
-              `E aí astronautas de <@&805765119384616962>, PR de **${projectName}** aberto há **${openSinceXHoursOrMinutes}h**, ninguém vai se alocar não? :angry:`,
-              `<@&805765119384616962>, preciso de alguém pra corrigir o PR **${discordId}** lá em **${projectName}**`,
-              `<@&805765119384616962>, por favor né, o tempo tá passando... Ninguém vai corrigir o PR de **${userGithubNick}** lá em **${projectName}** não? :rage:`,
-              `<@&805765119384616962>, preciso de alguém pra corrigir o PR **${discordId}** lá em **${projectName}**`,
-              `Galerinha querida de <@&805765119384616962>, se ninguém se alocar no PR **${discordId}** de **${projectName}** eu mesmo vou alocar viu, não me desafiem! :face_with_symbols_over_mouth:`,
-              `<@&805765119384616962>, preciso de alguém pra corrigir o PR **${discordId}** lá em **${projectName}**`,
+              `Já desisti de vcs viu <@&805765119384616962>, se aloquem ou não, não vou ficar mais de :clown: aqui...\nps: se existir alguma alma caridosa, seguem os dados: PR **${discordId}** de **${projectName}** aberto por **${userGithubNick}** com link ${githubLink}`,
+              `<@&805765119384616962>, preciso de alguém pra corrigir o PR **${discordId}** lá em **${projectName}** lá em ${githubLink}`,
+              `E aí astronautas de <@&805765119384616962>, PR de **${projectName}** aberto há **${openSinceXHoursOrMinutes}h**, ninguém vai se alocar não? :angry:\nOlha o link: ${githubLink}`,
+              `<@&805765119384616962>, preciso de alguém pra corrigir o PR **${discordId}** lá em **${projectName}** lá em ${githubLink}`,
+              `<@&805765119384616962>, por favor né, o tempo tá passando... Ninguém vai corrigir o PR de **${userGithubNick}** lá em **${projectName}** não? :rage:\nOlha o link: ${githubLink}`,
+              `<@&805765119384616962>, preciso de alguém pra corrigir o PR **${discordId}** lá em **${projectName}** lá em ${githubLink}`,
+              `Galerinha querida de <@&805765119384616962>, se ninguém se alocar no PR **${discordId}** de **${projectName}** eu mesmo vou alocar viu, não me desafiem! :face_with_symbols_over_mouth:\nOlha o link: ${githubLink}`,
+              `<@&805765119384616962>, preciso de alguém pra corrigir o PR **${discordId}** lá em **${projectName}** lá em ${githubLink}`,
             ],
             urgent: [
-              `<@&805765119384616962>, o PR **${discordId}** de **${projectName}** tá marcado como urgente, então corram pra se alocar e revisar!!!`,
-              `<@&805765119384616962>, vocês entendem o conceito da palavra URGENTE? Caso não, lidem com os clientes de **${projectName}** no lugar de **${userGithubNick}**, pq eles querem o PR **${discordId}** em prod pra ontem!!!`,
+              `<@&805765119384616962>, o PR **${discordId}** de **${projectName}** tá marcado como urgente, então corram pra se alocar e revisar!!!\nOlha o link: ${githubLink}`,
+              `<@&805765119384616962>, vocês entendem o conceito da palavra URGENTE? Caso não, lidem com os clientes de **${projectName}** no lugar de **${userGithubNick}**, pq eles querem o PR **${discordId}** em prod pra ontem!!!\nOlha o link: ${githubLink}`,
             ],
           };
           const messagesForAllocated = {
             important: [
-              `<@!${user?.discordId}> sem querer pressionar, mas já precionando...\nO PR de **${projectName}** continua aberto... :grimacing:\nSe não conseguir corrigir agora, tu pode passar o PR pra outra pessoa tá, e tá tudo bem!\n`,
-              `Contato! Repetindo, contato! <@!${user?.discordId}> existe um PR em **${projectName}** no qual você está alocado para revisão!`,
-              `Fala <@!${user?.discordId}> meu queride, lembra que tu tais alocado no PR de **${projectName}**, blz?? Dá uma conferida lá quando puder...\n||ps: o quanto antes melhor||`,
-              `Contato! Repetindo, contato! <@!${user?.discordId}> existe um PR em **${projectName}** no qual você está alocado para revisão!`,
-              `<@!${user?.discordId}> só pra relembrar que tu se alocou pra corrigir o PR lá em **${projectName}**, visse!?`,
-              `Contato! Repetindo, contato! <@!${user?.discordId}> existe um PR em **${projectName}** no qual você está alocado para revisão!`,
-              `<@!${user?.discordId}> :rose::rose: são vermelhas e violetas são azuis, mas mesmo assim o PR de **${projectName}** continua esperando sua revisão... :sweat: :wilted_rose:`,
-              `Contato! Repetindo, contato! <@!${user?.discordId}> existe um PR em **${projectName}** no qual você está alocado para revisão!`,
+              `<@!${user?.discordId}> sem querer pressionar, mas já precionando...\nO PR de **${projectName}** continua aberto... :grimacing:\nSe não conseguir corrigir agora, tu pode passar o PR pra outra pessoa tá, e tá tudo bem!\nOlha o link: ${githubLink}`,
+              `Contato! Repetindo, contato! <@!${user?.discordId}> existe um PR em **${projectName}** no qual você está alocado para revisão!\nOlha o link: ${githubLink}`,
+              `Fala <@!${user?.discordId}> meu queride, lembra que tu tais alocado no PR de **${projectName}**, blz?? Dá uma conferida lá quando puder...\nOlha o link: ${githubLink}\n||ps: o quanto antes melhor||`,
+              `Contato! Repetindo, contato! <@!${user?.discordId}> existe um PR em **${projectName}** no qual você está alocado para revisão!\nOlha o link: ${githubLink}`,
+              `<@!${user?.discordId}> só pra relembrar que tu se alocou pra corrigir o PR lá em **${projectName}**, visse!?\nOlha o link: ${githubLink}`,
+              `Contato! Repetindo, contato! <@!${user?.discordId}> existe um PR em **${projectName}** no qual você está alocado para revisão!\nOlha o link: ${githubLink}`,
+              `<@!${user?.discordId}> :rose::rose: são vermelhas e violetas são azuis, mas mesmo assim o PR de **${projectName}** continua esperando sua revisão... :sweat: :wilted_rose:\nOlha o link: ${githubLink}`,
+              `Contato! Repetindo, contato! <@!${user?.discordId}> existe um PR em **${projectName}** no qual você está alocado para revisão!\nOlha o link: ${githubLink}`,
             ],
             urgent: [
               `${
                 user?.discordId ? `<@!${user?.discordId}>` : user?.name
-              }, vc já está a frente do rolê só por ter se alocado!\nMas é que o PR tá marcado como urgente :grimacing:\nTem como ver isso o quanto antes?`,
-              `<@!${user?.discordId}> meu queride, eu sei que tá corrido por aí, mas tá corrido pra ${userGithubNick} tbm (principalmente com esse PR específico :grimacing:)\nTem como tu dar aquele gás pra a gente fechar esse PR?`,
-              `<@!${user?.discordId}> sem querer pressionar, mas tô aqui pra isso né...\nO PR de **${projectName}** continua aberto... :grimacing:\nSe não conseguir corrigir agora, tu pode passar o PR pra outra pessoa tá, e tá tudo bem!\nÉ só que, como o PR é urgente, a gente precisa daquele último gás sabe :sweat:`,
-              `Ou pessoal de <@&805765119384616962>, ninguém consegue ajudar <@!${user?.discordId}> com esse PR não? Parece que tá bem punk pra elu... :grimacing:\nPra rolar essa troca de reviewer é só outra pessoa rodar \`/me_aloca_aqui ${discordId}\` :wink:`,
+              }, vc já está a frente do rolê só por ter se alocado!\nMas é que o PR tá marcado como urgente :grimacing:\nTem como ver isso o quanto antes?\nOlha o link: ${githubLink}`,
+              `<@!${user?.discordId}> meu queride, eu sei que tá corrido por aí, mas tá corrido pra ${userGithubNick} tbm (principalmente com esse PR específico :grimacing:)\nTem como tu dar aquele gás pra a gente fechar esse PR?\nOlha o link: ${githubLink}`,
+              `<@!${user?.discordId}> sem querer pressionar, mas tô aqui pra isso né...\nO PR de **${projectName}** continua aberto... :grimacing:\nSe não conseguir corrigir agora, tu pode passar o PR pra outra pessoa tá, e tá tudo bem!\nÉ só que, como o PR é urgente, a gente precisa daquele último gás sabe :sweat:\nOlha o link: ${githubLink}`,
+              `Ou pessoal de <@&805765119384616962>, ninguém consegue ajudar <@!${user?.discordId}> com esse PR não? Parece que tá bem punk pra elu... :grimacing:\nPra rolar essa troca de reviewer é só outra pessoa rodar \`/me_aloca_aqui ${discordId}\` :wink:\nOlha o link: ${githubLink}`,
             ],
           };
           const messageToSend =
