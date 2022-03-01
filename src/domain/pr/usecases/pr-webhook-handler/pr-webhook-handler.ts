734b5411925448e646facc4809a1dbe579de0a81
argas7 <argas@cin.ufpe.br>
2022-03-01 18:25:35 -0300

    feat: add new business rule to pr-webhook-handler

--- src/domain/pr/usecases/pr-webhook-handler/pr-webhook-handler.ts
+++ src/domain/pr/usecases/pr-webhook-handler/pr-webhook-handler.ts
@@ -1,26 +1,32 @@
+import { SendMessageUsecase } from '@/domain/message/usecases/send-message';
 import { makePinoLoggerLocalAdapter } from '@/main/factories/infra/logs/pino';
 import { CreatePrUsecase } from '../create-pr-in-database/protocols';
 import { ListPrsFromDatabaseRepository } from '../list-prs-from-database/protocols';
 import { UpdatePrFromWebhookCallUsecase } from '../update-pr-from-webhook-call/protocols';
+import { PrWebhookHandlerServiceError } from './errors';
 import { PrWebhookHandler } from './protocols';
 // import { PrWebhookHandlerServiceError } from './errors';
 
 type CreatePrInDatabaseServiceInjectables = {
+  sendMessageUsecase: SendMessageUsecase;
   createPrFromWebhookCall: CreatePrUsecase;
   updatePrFromWebhookCall: UpdatePrFromWebhookCallUsecase;
   listPrsFromDatabaseRepository: ListPrsFromDatabaseRepository;
 };
 
 class PrsWebhookHandler implements PrWebhookHandler {
+  private readonly sendMessageUsecase: SendMessageUsecase;
   private readonly createPrFromWebhookCall: CreatePrUsecase;
   private readonly updatePrFromWebhookCall: UpdatePrFromWebhookCallUsecase;
   private readonly listPrsFromDatabaseRepository: ListPrsFromDatabaseRepository;
 
   constructor({
+    sendMessageUsecase,
     createPrFromWebhookCall,
     updatePrFromWebhookCall,
     listPrsFromDatabaseRepository,
   }: CreatePrInDatabaseServiceInjectables) {
+    this.sendMessageUsecase = sendMessageUsecase;
     this.createPrFromWebhookCall = createPrFromWebhookCall;
     this.listPrsFromDatabaseRepository = listPrsFromDatabaseRepository;
     this.updatePrFromWebhookCall = updatePrFromWebhookCall;
@@ -28,6 +34,21 @@ class PrsWebhookHandler implements PrWebhookHandler {
 
   async createOrUpdatePr(params: PrWebhookHandler.Params): Promise<void> {
     try {
+      const { origin } = params;
+
+      console.log('HTTP REQUEST');
+      console.log(params);
+
+      if (origin !== 'back' && origin !== 'front' && origin !== undefined) {
+        this.sendMessageUsecase.send({
+          content: `Ohhh <@&805765119384616962>, algum engraçadinho configurou errado o PAYLOAD do projeto **${params?.repository.name}**. Assim não dá pra fazer a mágica né... Alguém corrige lá, por favor :rolling_eyes:\nOlha o link: https://github.com/${params?.repository.full_name}/settings/hooks`,
+        });
+
+        throw new PrWebhookHandlerServiceError(
+          `Wrong webhook payload from ${params?.repository?.name}`
+        );
+      }
+
       const githubId = `${params.pull_request?.id}`;
 
       const { prs } = await this.listPrsFromDatabaseRepository.listPrs({
